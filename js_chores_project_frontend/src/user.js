292c5da8600c80d110d32a5b685564e9a41d80f0
tibraj <tibraj94@gmail.com>
2020-10-03 15:58:53 -0400

    function for creating new user

--- js_chores_project_frontend/src/user.js
+++ js_chores_project_frontend/src/user.js
@@ -5,3 +5,22 @@ class User {
         this.chores = user.chores;
     }
 }
+
+function createUser(e) {
+    e.preventDefault;
+    const user = {
+        name: document.getElementById('name').value
+    }
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+        body: JSON.stringify(user), 
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        const newUser = new User(json);
+    })
+}
