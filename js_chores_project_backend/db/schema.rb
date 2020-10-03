9f43ee13f73ecc09003e45a3959f790fd4a3c5c4
tibraj <tibraj94@gmail.com>
2020-10-03 13:36:10 -0400

    migrated chores table

--- js_chores_project_backend/db/schema.rb
+++ js_chores_project_backend/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_173308) do
+ActiveRecord::Schema.define(version: 2020_10_03_173531) do
+
+  create_table "chores", force: :cascade do |t|
+    t.text "task"
+    t.integer "duration"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_chores_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_10_03_173308) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "chores", "users"
 end
