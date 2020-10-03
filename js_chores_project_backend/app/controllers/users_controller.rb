479e1ec71886e561bd1f7381b40c47612514bd6a
tibraj <tibraj94@gmail.com>
2020-10-03 16:33:24 -0400

    fixed syntax

--- js_chores_project_backend/app/controllers/users_controller.rb
+++ js_chores_project_backend/app/controllers/users_controller.rb
@@ -7,13 +7,13 @@ class UsersController < ApplicationController
 
     def show 
         user = User.find(params[:id])
-        render json :user
+        render json: user
     end
 
     def create
         user = User.new(user_params)
         if user.save 
-            render json :user 
+            render json: user 
         else 
             render json: {errors: user.errors.full_messages}
         end 
