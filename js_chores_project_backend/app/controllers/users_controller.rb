229ad045c874f90c0a754a17ec03f24b34864b3e
tibraj <tibraj94@gmail.com>
2020-10-03 14:33:03 -0400

    private method for user params

--- js_chores_project_backend/app/controllers/users_controller.rb
+++ js_chores_project_backend/app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
     def show 
         user = User.find(params[:id])
         render json :user
-   end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 
 end
