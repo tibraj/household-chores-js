b818b53d0ecbee1dcfdcfd1a116520e3bef55040
tibraj <tibraj94@gmail.com>
2020-10-03 14:35:02 -0400

    added create action

--- js_chores_project_backend/app/controllers/users_controller.rb
+++ js_chores_project_backend/app/controllers/users_controller.rb
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
         render json :user
     end
 
+    def create
+        user = User.new(user_params)
+        if user.save 
+            render json :user 
+        else 
+            render json: {errors: user.errors.full_messages}
+        end 
+    end
+
     private
 
     def user_params
