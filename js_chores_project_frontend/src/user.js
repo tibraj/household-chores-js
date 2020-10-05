874d42cca771ade7b2ce9cf3a671799647573782
tibraj <tibraj94@gmail.com>
2020-10-05 12:11:14 -0400

    reformatted event listener for loading document

--- js_chores_project_frontend/src/user.js
+++ js_chores_project_frontend/src/user.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    User.createUser();
-})
-
 class User {
     constructor(user) {
         this.id = user.id;
@@ -9,35 +5,24 @@ class User {
         this.chores = user.chores;
     }
 
-    static userForm(user) {
-        document.getElementById("new-user").addEventListener("submit", function(e) {
-            e.preventDefault();
-            const userName = {
-                name: document.getElementById('name').value
-            }
-            fetch("http://localhost:3000/users", {
-            method: 'POST',
-            body: JSON.stringify(userName), 
-            headers: {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            }
-            })
-            .then(resp => resp.json())
-            .then(json => {
-                const newUser = new User(json);
-                newUser.userHeader();
-            })
-        })
-    }
-
-    userHeader() {
-        let container = document.getElementById('container');
-        container.innerHTML = '';
-        let userHeading = document.getElementById('user-header');
-        userHeading.innerHTML = `<h1>${this.name}'s Chores:</h1>`
+    static userForm(){
+        let newForm = document.getElementById('new-user-form');
+        newForm.innerHTML += `
+            <form id='new-user'>
+                <label>Enter Name:</label>
+                <input type="text" id="name" placeholder="Please Enter Your Name">
+                <input type="submit">
+            </form>
+        `
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    User.userForm();
+    console.log('Success');
+})
+
 
 
 
