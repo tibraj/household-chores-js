abdb4366bc35bd2eac1ee14dd97ee780526c2118
tibraj <tibraj94@gmail.com>
2020-10-05 13:51:47 -0400

    created chore object

---
+++ js_chores_project_frontend/src/chore.js
@@ -0,0 +1,13 @@
+class Chore {
+    constructor(chore) {
+        this.id = chore.id;
+        this.task = chore.task;
+        this.duration = chore.duration;
+        this.user_id = chore.user_id
+    }
+
+   
+}
+
+
+
