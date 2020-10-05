1e473a617fe19c197533ca0472a15e7e4e1c764e
tibraj <tibraj94@gmail.com>
2020-10-05 13:52:08 -0400

    static method for new chore form

--- js_chores_project_frontend/src/chore.js
+++ js_chores_project_frontend/src/chore.js
@@ -6,7 +6,18 @@ class Chore {
         this.user_id = chore.user_id
     }
 
-   
+    static newChoreForm() {
+        let newChoreForm = document.getElementById('new-chore-form');
+        newChoreForm.innerHTML += `
+            <form id='new-chore'>
+                <label>Enter Task:</label>
+                <input type="text" id="task" placeholder="Please Enter Task">
+                <label>Enter Chore Duration (in minutes):</label>
+                <input type="number" id="duration" placeholder="Enter a Number">
+                <input type="submit">
+            </form>
+        `
+    }
 }
 
 
